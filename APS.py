04670f1f736a981c985860c80fbb4ecbe9ab1af6
Caio Aranha <caranhatieri@gmail.com>
2023-03-03 09:31:19 -0300

    asdasda

--- APS.py
+++ APS.py
@@ -19,7 +19,7 @@ Tinf =  273+25 #K
 Tb = 373 #K
 m = math.sqrt((h*p)/(k*a)) #m^2/s   
 
-#CALCULOS
+#CALCULOS EX1------------------------------------------------------------------------------------------
 theta = math.cosh(0) + (h/(m*k))*math.sinh(0)
 
 thetab= math.cosh(m*l) + (h/(m*k))*math.sinh(m*l)
@@ -33,10 +33,10 @@ print(delta)
 Tl = delta*(Tb-Tinf) + Tinf
 print("Tl = ",Tl,"K")
 
-
+# Calculando a distribuicao
 listaTl = []
-for l in np.arange(0,0.21,0.01):
-    thetab= math.cosh(m*l) + (h/(m*k))*math.sinh(m*l)
+for x in np.arange(0,0.21,0.01):
+    theta= math.cosh(m*(l-x)) + (h/(m*k))*math.sinh(m*(l-x))
     delta = theta/thetab
     Tl = delta*(Tb-Tinf) + Tinf
     listaTl.append(Tl)
@@ -53,5 +53,29 @@ plt.xlabel("l(m)")
 plt.ylabel("Tl(K)")
 
 plt.show()
+
+#CALCULOS EX2------------------------------------------------------------------------------------------
+
+M = (Tb-Tinf)*(math.sqrt(h*p*k*a))
+print("M = ",M)
+
+qa = M*((math.sinh(m*l) + (h/(m*k))*math.cosh(m*l))/(math.cosh(m*l) + (h/(m*k))*math.sinh(m*l)))
+
+print("qa = ",qa)
+
+#CALCULOS EX3------------------------------------------------------------------------------------------
+ef = qa/(h*a*thetab)
+
+print("eficiencia = ",ef)
+
+#CALCULOS EX4------------------------------------------------------------------------------------------
+As = a + (2 * math.pi * (D/2)) * l # area da superficie
+efet = qa/(h*As*(thetab))
+
+print("efetividade = ",efet)
+
+#CALCULOS EX5------------------------------------------------------------------------------------------
+
+
 
 
