21f9c97f0fe66d01d597e1f22150da3664c52695
Felipe Menotti Trintim <felipemt3@al.insper.edu.br>
2023-03-07 17:10:24 -0300

    feito

--- APS.py
+++ APS.py
@@ -19,6 +19,7 @@ Tinf =  273+25 #K
 Tb = 373 #K
 m = math.sqrt((h*p)/(k*ab)) #m^2/s   
 
+#QUESTAO 1 ------------------------------------------------------------------------------------------
 #CALCULOS EX1------------------------------------------------------------------------------------------
 
 theta = math.cosh(0) + (h/(m*k))*math.sinh(0)
@@ -32,7 +33,7 @@ delta = theta/thetab
 # print(delta)
 
 Tl = delta*(Tb-Tinf) + Tinf
-print("Tl = ",round(Tl, 2),"K")
+print("Temperatura na extremidade da aleta = ",round(Tl, 2),"K")
 
 # Calculando a distribuicao
 listaTl = []
@@ -62,25 +63,25 @@ print("M = ",round(M, 2))
 
 qa = M*((math.sinh(m*l) + (h/(m*k))*math.cosh(m*l))/(math.cosh(m*l) + (h/(m*k))*math.sinh(m*l)))
 
-print("qa = ",round(qa, 2))
+print("Taxa de transferencia de calor (qa) = ",round(qa, 2))
 
 #CALCULOS EX3------------------------------------------------------------------------------------------
 
 As = ab + (2 * math.pi * (D/2)) * l # area da superficie
 eficiencia = qa/(h*As*(Tb-Tinf))
 
-print("eficiencia = ",round(eficiencia*100, 2), "%")
+print("eficiencia da aleta= ",round(eficiencia*100, 2), "%")
 
 #CALCULOS EX4------------------------------------------------------------------------------------------
 
 efetividade = qa/(h*ab*(Tb-Tinf))
-print("efetividade = ", round(efetividade, 2))
+print("efetividade da aleta = ", round(efetividade, 2))
 
 #CALCULOS EX5------------------------------------------------------------------------------------------
 
 #para aleta infinita qa = a M
 qa_infinita = M
-print("qa aleta infinita = ",round(qa_infinita, 2))
+print("Taxa de transferencia de calor na aleta infinita (qa_inf)= ",round(qa_infinita, 2))
 
 #CALCULOS EX6------------------------------------------------------------------------------------------
 
@@ -121,3 +122,32 @@ while (erro > tole):
     l += 0.001
 
 print("tamanho da aleta para ser igual a hipotese da aleta infinita = ",round(l, 2), "m")	
+
+#QUESTAO 2 ------------------------------------------------------------------------------------------
+#CALCULOS EX1 ------------------------------------------------------------------------------------------
+
+def calcula_k_aleta_infinita(Tb, TA, TB, Tinf, h, p, ab, ka):
+    theta_A = (TA - Tinf)
+    theta_B = (TB - Tinf)
+    theta_b = (Tb - Tinf)
+    m = math.sqrt((h*p)/(ka*ab))
+    x = math.log(theta_A/theta_b)/(-m)
+    kb = ((h*p)/ab)*(1/((math.log(theta_A/theta_B)/x)+ math.sqrt((h*p)/(ka*ab)))**2)
+    return kb
+
+#CALCULOS EX2 ------------------------------------------------------------------------------------------
+ 
+ka = 240
+TA = 273 + 75
+TB = 273 + 60
+Tb = 273 + 100
+Tinf = 273 + 25
+ab = math.pi*r**2 
+p = math.pi * 2*r  
+h = 25 
+
+kb = calcula_k_aleta_infinita(Tb, TA, TB, Tinf, h, p, ab, ka)
+print("kb da aleta infinita = ",round(kb, 2), "W/m.K")
+
+
+
